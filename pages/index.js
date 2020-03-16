1afb94784638132a60fc214da057bcb0cf13b9fa
Laurens OB <laurensortiz@laurensob.local>
2020-03-15 23:26:03 -0600

    changed migration order

--- pages/index.js
+++ pages/index.js
@@ -19,9 +19,11 @@ class Index extends Component {
     return (
       <Row align="middle" type="flex" justify="center" className="login-container">
         <video alt="WebTrader"
-               src="/static/bg-intro-video.mp4"
-               poster="/static/bg-intro.gif"
-               autoPlay={true} loop={true} className="intro-video" playbackRate={0.5}></video>
+               src="./static/bg-intro-video.mp4"
+               poster="./static/bg-intro.gif"
+               autoPlay={true} loop={true} className="intro-video" playbackRate="0.5"
+               style={{position: 'fixed', top:0, left:0, right:0, bottom:0, minWidth: '100%', minHeight: '100%', opacity: .1, zIndex:0}}
+        />
         <Col xs={24} md={12} lg={12} className="login-form">
           <AuthLoginForm />
         </Col>
