b9083e7352b17a3df1352b4476f65f510a51fbe4
Laurens OB <laurensortiz@laurensob.local>
2020-03-30 11:26:24 -0600

    Added log

--- common/utils/format-axios-error.js
+++ common/utils/format-axios-error.js
@@ -4,6 +4,10 @@ const formatAxiosError = error => {
   const status = _.get(error, 'status', 500);
   const context = _.get(error, 'data.name', 'Tipo de error no registrado');
   const message = _.get(error, 'data.errors[0].message', 'Falla en solicitud');
+  
+  console.log('[=====  ERROR  =====>');
+  console.log(error);
+  console.log('<=====  /ERROR  =====]');
 
   return `Error [${status}]: Tipo: ${context} | Detalle: ${message}`;
 };
