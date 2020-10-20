99a917a5752824b7fc74bb9576dd8919922ece3e
Laurens OB <laurensortiz@laurensob.local>
2020-10-20 08:07:47 -0600

    lang

---
+++ intl/lang.js
@@ -0,0 +1,14 @@
+export const Lang = {
+  en: {
+    about: "About",
+    articles: "Articles",
+    home: "Home",
+    slogan: "An example site showcasing a bilingual site with GatsbyJS.",
+  },
+  es: {
+    about: "Sobre",
+    articles: "Artigos",
+    home: "Início",
+    slogan: "Um site bilíngue de exemplo feito com GatsbyJS.",
+  },
+}
