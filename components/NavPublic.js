080b7ebf9cb9bc2a2f61ea45aa54a7d38121e461
Laurens OB <laurensortiz@laurensob.local>
2021-03-24 11:31:29 -0600

    Added new page Meta Trader

--- components/NavPublic.js
+++ components/NavPublic.js
@@ -1,38 +1,62 @@
-import React, { PureComponent } from 'react';
-import { Icon } from 'antd';
-import { withNamespaces } from 'react-i18next';
+import React, { PureComponent } from 'react'
+import { Icon, Badge } from 'antd'
+import { withNamespaces } from 'react-i18next'
 
-
-import Link from '../components/Link';
-import LangSelector from './LangSelector';
+import Link from '../components/Link'
+import LangSelector from './LangSelector'
 
 class NavPublic extends PureComponent {
-
   render() {
-    const {t} = this.props;
+    const { t } = this.props
 
     return (
-
-      <ul className="main-menu"
-      >
+      <ul className="main-menu">
         <li>
-          <Link className="menu-item" href="/market"><Icon type="sliders"/> <span>{t('mainMenu market')}</span></Link>
+          <Link className="menu-item" href="/metatrader">
+            <Icon type="sliders" />{' '}
+            <span>
+              MetaTrader 5{' '}
+              <Badge
+                count="Nuevo"
+                style={{
+                  backgroundColor: '#1653d1',
+                  fontSize: 10,
+                  height: 18,
+                  lineHeight: 2,
+                  top: -3,
+                  color: '#fff',
+                  letterSpacing: 1,
+                }}
+              />
+            </span>
+          </Link>
         </li>
         <li>
-          <Link className="menu-item" href="/calendar"><Icon type="fund"/> <span>{t('mainMenu economicCalendar')}</span></Link>
+          <Link className="menu-item" href="/market">
+            <Icon type="stock" /> <span>{t('mainMenu market')}</span>
+          </Link>
         </li>
         <li>
-          <Link className="menu-item" href="/user-operations"><Icon type="area-chart" /> <span>{t('mainMenu operations')}</span></Link>
+          <Link className="menu-item" href="/calendar">
+            <Icon type="fund" /> <span>{t('mainMenu economicCalendar')}</span>
+          </Link>
         </li>
         <li>
-          <Link className="menu-item" onClick={ this.props.onLogout } href="/"><Icon type="logout"/> <span>{t('mainMenu logout')}</span></Link>
+          <Link className="menu-item" href="/user-operations">
+            <Icon type="area-chart" /> <span>{t('mainMenu operations')}</span>
+          </Link>
+        </li>
+        <li>
+          <Link className="menu-item" onClick={this.props.onLogout} href="/">
+            <Icon type="logout" /> <span>{t('mainMenu logout')}</span>
+          </Link>
         </li>
         <li>
           <LangSelector />
         </li>
       </ul>
-    );
+    )
   }
 }
 
-export default withNamespaces()(NavPublic);
+export default withNamespaces()(NavPublic)
--- components/WireTransferRequest/AddOrEditForm.js
+++ components/WireTransferRequest/AddOrEditForm.js
@@ -194,9 +194,6 @@ class AddOrEditForm extends PureComponent {
 
   handleInversion = (rule, value, callback) => {
     const regex = /^[1-9]\d*(((,\d{3}){1})?(\.\d{0,2})?)$/
-    console.log('[=====    =====>')
-    console.log(this.state.userAccount)
-    console.log('<=====  /  =====]')
     const userStartedDay = this.state.userAccount.user.startDate
     const { associatedOperation, percentage } = this.state.userAccount.account
     const isOTCAccount = associatedOperation === 1
