db9557d8155b2c334210446f957363de95099390
Laurens OB <laurensortiz@Laurenss-MacBook-Pro.local>
2021-04-20 12:27:04 -0600

    Added new product

--- components/Operation/Market/BulkUpdateSteps.js
+++ components/Operation/Market/BulkUpdateSteps.js
@@ -1,112 +1,117 @@
-import React, { useState, useEffect } from "react";
-import { isEmpty, split } from "lodash";
-import { Steps, Button, Icon, Tag, Select, Input, Result, Radio } from "antd";
-import classNames from "classnames";
-const { Step } = Steps;
-const { Option } = Select;
+import React, { useState, useEffect } from 'react'
+import { isEmpty, split } from 'lodash'
+import { Steps, Button, Icon, Tag, Select, Input, Result, Radio } from 'antd'
+import classNames from 'classnames'
+const { Step } = Steps
+const { Option } = Select
 
 const BULK_UPDATE_TYPES = [
   {
-    code: "report",
-    name: "Generar reporte de operaciones",
-    scope: "report",
+    code: 'report',
+    name: 'Generar reporte de operaciones',
+    scope: 'report',
   },
   {
-    code: "status",
-    name: "Estado",
-    scope: "status",
+    code: 'status',
+    name: 'Estado',
+    scope: 'status',
   },
   {
-    code: "stopLost",
-    name: "Stop Loss",
-    scope: "stopLost",
+    code: 'stopLost',
+    name: 'Stop Loss',
+    scope: 'stopLost',
   },
   {
-    code: "takingProfit",
-    name: "Taking Profit",
-    scope: "takingProfit",
+    code: 'takingProfit',
+    name: 'Taking Profit',
+    scope: 'takingProfit',
   },
   {
-    code: "stocks",
-    name: "Stocks",
-    scope: "price",
+    code: 'stocks',
+    name: 'Stocks',
+    scope: 'price',
   },
   {
-    code: "gold-FU-OP",
-    name: "GOLD Futures - Options",
-    scope: "price",
+    code: 'gold-FU-OP',
+    name: 'GOLD Futures - Options',
+    scope: 'price',
   },
   {
-    code: "gold-CFD-Ounces",
-    name: "GOLD CFD Ounces",
-    scope: "price",
+    code: 'gold-CFD-Ounces',
+    name: 'GOLD CFD Ounces',
+    scope: 'price',
   },
   {
-    code: "gold-Spot-XAU",
-    name: "GOLD Spot XAU/USD",
-    scope: "price",
+    code: 'gold-Spot-XAU',
+    name: 'GOLD Spot XAU/USD',
+    scope: 'price',
   },
   {
-    code: "silver-FT-OP",
-    name: "SILVER Futures - Options",
-    scope: "price",
+    code: 'silver-FT-OP',
+    name: 'SILVER Futures - Options',
+    scope: 'price',
   },
   {
-    code: "silver-CFD-Ounces",
-    name: "SILVER CFD Ounces",
-    scope: "price",
+    code: 'silver-CFD-Ounces',
+    name: 'SILVER CFD Ounces',
+    scope: 'price',
   },
   {
-    code: "platinum-FT-OP",
-    name: "PLATINUM Futures - Options",
-    scope: "price",
+    code: 'platinum-FT-OP',
+    name: 'PLATINUM Futures - Options',
+    scope: 'price',
   },
   {
-    code: "platinum-CFD-Ounces",
-    name: "PLATINUM CFD Ounces",
-    scope: "price",
+    code: 'platinum-CFD-Ounces',
+    name: 'PLATINUM CFD Ounces',
+    scope: 'price',
   },
   {
-    code: "crudeOil-FT-OP",
-    name: "CRUDE OIL WTI Futures - Options",
-    scope: "price",
+    code: 'crudeOil-FT-OP',
+    name: 'CRUDE OIL WTI Futures - Options',
+    scope: 'price',
   },
   {
-    code: "crude-CFDs-Barrels",
-    name: "CRUDE CFDs Barrels",
-    scope: "price",
+    code: 'crude-CFDs-Barrels',
+    name: 'CRUDE CFDs Barrels',
+    scope: 'price',
   },
   {
-    code: "us-Wheat-Contract",
-    name: "US Wheat Contract",
-    scope: "price",
+    code: 'us-Wheat-Contract',
+    name: 'US Wheat Contract',
+    scope: 'price',
   },
   {
-    code: "cooper-FT-OP",
-    name: "Cooper",
-    scope: "price",
+    code: 'cooper-FT-OP',
+    name: 'Cooper',
+    scope: 'price',
   },
   {
-    code: "cbo-FT-OP",
-    name: "CBO",
-    scope: "price",
+    code: 'cbo-FT-OP',
+    name: 'CBO',
+    scope: 'price',
   },
   {
-    code: "orange-FT-OP",
-    name: "Orange",
-    scope: "price",
+    code: 'orange-FT-OP',
+    name: 'Orange',
+    scope: 'price',
   },
   {
-    code: "natural-FT",
-    name: "Natural",
-    scope: "price",
+    code: 'natural-FT',
+    name: 'Natural',
+    scope: 'price',
   },
   {
-    code: "cocoa-FU-OP",
-    name: "Cocoa",
-    scope: "price",
+    code: 'cocoa-FU-OP',
+    name: 'Cocoa',
+    scope: 'price',
   },
-];
+  {
+    code: 'coffee-FT',
+    name: 'Coffee US FT ',
+    scope: 'price',
+  },
+]
 
 function BulkUpdateSteps({
   selectedElements,
@@ -115,68 +120,68 @@ function BulkUpdateSteps({
   isBulkLoading,
   isBulkSuccess,
 }) {
-  const [currentStep, setCurrentStep] = useState(0);
-  const [updateType, setUpdateType] = useState("");
-  const [updateScope, setUpdateScope] = useState("");
-  const [updateValue, setUpdateValue] = useState({});
-  const [behaviorClass, setBehaviorClass] = useState("");
-  const [isValidAmount, setIsValidAmount] = useState(true);
-  const [isValidOperation, setIsValidOperation] = useState(false);
+  const [currentStep, setCurrentStep] = useState(0)
+  const [updateType, setUpdateType] = useState('')
+  const [updateScope, setUpdateScope] = useState('')
+  const [updateValue, setUpdateValue] = useState({})
+  const [behaviorClass, setBehaviorClass] = useState('')
+  const [isValidAmount, setIsValidAmount] = useState(true)
+  const [isValidOperation, setIsValidOperation] = useState(false)
 
   useEffect(() => {
-    setIsValidAmount(true);
-    setBehaviorClass("");
-    setUpdateValue(null);
-  }, [updateType]);
+    setIsValidAmount(true)
+    setBehaviorClass('')
+    setUpdateValue(null)
+  }, [updateType])
 
   const _isValidAmount = (amount) => {
-    const regex = /^-?[0-9]\d*(((,\d{3}){1})?(\.\d{0,2})?)$/;
-    const isValid = regex.test(amount);
-    setIsValidAmount(isValid);
+    const regex = /^-?[0-9]\d*(((,\d{3}){1})?(\.\d{0,2})?)$/
+    const isValid = regex.test(amount)
+    setIsValidAmount(isValid)
     if (!isValid) {
-      setBehaviorClass("");
+      setBehaviorClass('')
     }
-    return isValid;
-  };
+    return isValid
+  }
 
   const _isPositiveAmount = (amount) => {
-    const isPositive = Math.sign(amount) >= 0;
-    setBehaviorClass(isPositive ? "positive-amount" : "negative-amount");
-    return isPositive;
-  };
+    const isPositive = Math.sign(amount) >= 0
+    setBehaviorClass(isPositive ? 'positive-amount' : 'negative-amount')
+    return isPositive
+  }
 
   const _handlePriceInputChange = ({ target }) => {
-    if (target.name !== "marketPrice") {
-      _isPositiveAmount(target.value);
+    if (target.name !== 'marketPrice') {
+      _isPositiveAmount(target.value)
     }
 
-    _isValidAmount(target.value);
+    _isValidAmount(target.value)
 
     setUpdateValue({
       ...updateValue,
       [target.name]: target.value,
-    });
-  };
+    })
+  }
 
   const _handleSingleInputChange = ({ target }) => {
-    setUpdateValue(target.value);
-  };
+    setUpdateValue(target.value)
+  }
 
   const _onSelectActionType = (value) => {
-    const operationValue = split(value, "_");
+    const operationValue = split(value, '_')
 
-    setUpdateType(operationValue[0]);
-    setUpdateScope(operationValue[1]);
-  };
+    setUpdateType(operationValue[0])
+    setUpdateScope(operationValue[1])
+  }
 
   const _onRequestSaveOperation = (operationInfo) => {
-    setIsValidOperation(true);
-    setUpdateValue(operationInfo);
-  };
+    setIsValidOperation(true)
+    setUpdateValue(operationInfo)
+  }
 
   const _bulkUpdateValue = () => {
     switch (updateScope) {
-      case "status":
+      case 'status':
         return (
           <Radio.Group name="status" onChange={_handleSingleInputChange}>
             <Radio value={1}>
@@ -192,8 +197,8 @@ function BulkUpdateSteps({
               <Tag color="#414241">Vendido</Tag>
             </Radio>
           </Radio.Group>
-        );
-      case "stopLost":
+        )
+      case 'stopLost':
         return (
           <>
             <Input
@@ -206,8 +211,8 @@ function BulkUpdateSteps({
               placeholder="Stop Loss"
             />
           </>
-        );
-      case "takingProfit":
+        )
+      case 'takingProfit':
         return (
           <>
             <Input
@@ -220,8 +225,8 @@ function BulkUpdateSteps({
               placeholder="Taking Profit"
             />
           </>
-        );
-      case "price":
+        )
+      case 'price':
         return (
           <>
             <Input
@@ -242,21 +247,19 @@ function BulkUpdateSteps({
               placeholder="MP"
             />
           </>
-        );
+        )
       default:
-        return (
-          <div className="no-visible" style={{ width: 150, height: 40 }} />
-        );
+        return <div className="no-visible" style={{ width: 150, height: 40 }} />
     }
-  };
+  }
 
   const _selectUpdateType = () => (
-    <div style={{ textAlign: "center" }}>
+    <div style={{ textAlign: 'center' }}>
       <Select
         showSearch={true}
         size="large"
         defaultValue=""
-        style={{ minWidth: "20%" }}
+        style={{ minWidth: '20%' }}
         onChange={_onSelectActionType}
       >
         <Option key="0" value="">
@@ -270,34 +273,26 @@ function BulkUpdateSteps({
       </Select>
       <div style={{ marginTop: 20, minHeight: 40 }}>
         {_bulkUpdateValue()}
-        {!isValidAmount ? (
-          <p className="m-t-20 negative">Monto inválido</p>
-        ) : null}
+        {!isValidAmount ? <p className="m-t-20 negative">Monto inválido</p> : null}
       </div>
     </div>
-  );
+  )
 
   const _lastStepContent = () => {
     const title = isProcessComplete ? (
       isBulkSuccess ? (
         <span>
-          <Tag color="#046d11">{selectedElements}</Tag>Productos actualizados
-          correctamente
+          <Tag color="#046d11">{selectedElements}</Tag>Productos actualizados correctamente
         </span>
       ) : (
-        "Ocurrió un error"
+        'Ocurrió un error'
       )
     ) : (
       <span>
-        Proceder con la actualización de{" "}
-        <Tag color="#046d11">{selectedElements}</Tag>productos
+        Proceder con la actualización de <Tag color="#046d11">{selectedElements}</Tag>productos
       </span>
-    );
-    const status = isProcessComplete
-      ? isBulkSuccess
-        ? "success"
-        : "error"
-      : "info";
+    )
+    const status = isProcessComplete ? (isBulkSuccess ? 'success' : 'error') : 'info'
 
     return (
       <Result
@@ -308,38 +303,35 @@ function BulkUpdateSteps({
             size="large"
             type="primary"
             key="console"
-            onClick={() =>
-              onClickUpdate({ updateType, updateValue, updateScope })
-            }
+            onClick={() => onClickUpdate({ updateType, updateValue, updateScope })}
             loading={isBulkLoading}
-            className={classNames({ "no-visible": isProcessComplete })}
+            className={classNames({ 'no-visible': isProcessComplete })}
           >
             Realizar actualización
           </Button>
         }
       />
-    );
-  };
+    )
+  }
 
   const steps = [
     {
-      title: "Seleccione las operaciones",
+      title: 'Seleccione las operaciones',
       content: (
         <h3>
-          Operaciones seleccionadas{" "}
-          <Tag color="#046d11">{selectedElements}</Tag>
+          Operaciones seleccionadas <Tag color="#046d11">{selectedElements}</Tag>
         </h3>
       ),
     },
     {
-      title: "Registro y Valor",
+      title: 'Registro y Valor',
       content: _selectUpdateType(),
     },
     {
-      title: "Confirmación",
+      title: 'Confirmación',
       content: _lastStepContent(),
     },
-  ];
+  ]
 
   return (
     <>
@@ -352,10 +344,7 @@ function BulkUpdateSteps({
 
       <div className="steps-action">
         {currentStep > 0 && (
-          <Button
-            style={{ marginRight: 15 }}
-            onClick={() => setCurrentStep(currentStep - 1)}
-          >
+          <Button style={{ marginRight: 15 }} onClick={() => setCurrentStep(currentStep - 1)}>
             <Icon type="left-square" /> Anterior
           </Button>
         )}
@@ -364,11 +353,9 @@ function BulkUpdateSteps({
             type="primary"
             onClick={() => setCurrentStep(currentStep + 1)}
             disabled={
-              (updateType !== "status" &&
-                !isValidAmount &&
-                isEmpty(updateValue)) ||
-              (updateType === "status" && !Number.isInteger(updateValue)) ||
-              (updateType === "create-operation" && !isValidOperation)
+              (updateType !== 'status' && !isValidAmount && isEmpty(updateValue)) ||
+              (updateType === 'status' && !Number.isInteger(updateValue)) ||
+              (updateType === 'create-operation' && !isValidOperation)
             }
           >
             Siguiente <Icon type="right-square" />
@@ -384,7 +371,7 @@ function BulkUpdateSteps({
         }
       `}</style>
     </>
-  );
+  )
 }
 
-export default BulkUpdateSteps;
+export default BulkUpdateSteps
