89ae27004112f7aff0ddfee91b5a3b827b31e181
Laurens OB <laurens.ortiz@gmail.com>
2021-07-13 08:14:48 -0600

    fix users

--- state/modules/users/api.js
+++ state/modules/users/api.js
@@ -1,31 +1,31 @@
-import fetch from '../../../common/fetch';
+import fetch from '../../../common/fetch'
 
-export const getUsers = async ({role = 2, status = 1}) => {
+export const getUsers = async ({ role = 2, status = 1 }) => {
   return fetch({
     method: 'get',
     url: `users/list/${role}/${status}`,
-  });
-};
+  })
+}
 
 export const addUser = async (user) => {
   return fetch({
     method: 'post',
     url: 'users',
-    data: user
-  });
-};
+    data: user,
+  })
+}
 
 export const editUser = async (user) => {
   return fetch({
     method: 'put',
     url: `users/${user.id}`,
-    data: user
-  });
-};
+    data: user,
+  })
+}
 
 export const deleteUser = async (userId) => {
   return fetch({
     method: 'delete',
     url: `users/${userId}`,
-  });
-};
+  })
+}
