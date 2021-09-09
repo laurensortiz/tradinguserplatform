8f723bccdb83a4fdc15055c5a3723d4699d27157
Laurens OB <laurens.ortiz@gmail.com>
2021-09-09 01:23:02 -0600

    timeout

--- server.js
+++ server.js
@@ -12,15 +12,47 @@ import setupAuth from './api/auth'
 import setupApi from './api'
 import enforceSSL from './common/enforce-ssl'
 
+var timeout = require('express-timeout-handler')
+
 const dev = process.env.NODE_ENV !== 'production'
 const port = parseInt(process.env.PORT, 10) || 8000
 const app = next({ dev, quiet: false, api: { externalResolver: true } })
 const nextRequestHandler = app.getRequestHandler()
 
+var options = {
+  // Optional. This will be the default timeout for all endpoints.
+  // If omitted there is no default timeout on endpoints
+  timeout: 480000,
+
+  // Optional. This function will be called on a timeout and it MUST
+  // terminate the request.
+  // If omitted the module will end the request with a default 503 error.
+  onTimeout: function (req, res) {
+    res.status(503).send('Service unavailable. Please retry.')
+  },
+
+  // Optional. Define a function to be called if an attempt to send a response
+  // happens after the timeout where:
+  // - method: is the method that was called on the response object
+  // - args: are the arguments passed to the method
+  // - requestTime: is the duration of the request
+  // timeout happened
+  onDelayedResponse: function (req, method, args, requestTime) {
+    console.log(`Attempted to call ${method} after timeout`)
+  },
+
+  // Optional. Provide a list of which methods should be disabled on the
+  // response object when a timeout happens and an error has been sent. If
+  // omitted, a default list of all methods that tries to send a response
+  // will be disable on the response object
+  //disable: ['write', 'setHeaders', 'send', 'json', 'end'],
+}
+
 app.prepare().then(() => {
   const server = express()
 
   if (!dev) {
+    server.use(timeout.handler(options))
     server.use(compression())
   }
 
